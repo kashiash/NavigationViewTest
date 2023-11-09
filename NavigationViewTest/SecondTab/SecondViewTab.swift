3d81e1e4d8f32f4ba57cb5b8a7070b40136c69fe
Jacek Kosinski <kashiash@gmail.com>
2023-11-09 14:56:05 +0100

    book list n

--- NavigationViewTest/SecondTab/SecondViewTab.swift
+++ NavigationViewTest/SecondTab/SecondViewTab.swift
@@ -9,21 +9,35 @@ import SwiftUI
 
 struct SecondViewTab: View {
     let books = Book.examples()
-    @State private var path = NavigationPath() // [Book]()
+    @State private var path =  [Book]()
     var body: some View {
         NavigationStack(path: $path) {
             List {
                 ForEach(books) { book in
                     NavigationLink(book.title,value:book)
                 }
+
             }
             .navigationDestination(for: Book.self) { book in
                 BookDetailView(book: book)
             }
+
+        }
+        VStack {
+            ForEach(path) {
+                book in
+                Text(book.title)
+            }
+            Button("Go to favorities", systemImage: "star.fill") {
+                guard let book = books.first else { return }
+                path = [book]
+            }
         }
     }
 }
-
 #Preview {
+    SecondViewTab()
+}
+#Preview(" fit", traits: .sizeThatFitsLayout) {
     SecondViewTab()
 }
